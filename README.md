61e0c59d9e33ec67980ff7e4c61eaeea96180ed9
Vikram <svk.vikram@gmail.com>
2024-06-17 00:00:26 +0530

    Commit initial setup

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # CustomersService
-Spring boot application
---
+++ src/main/java/com/vikram/customers/CustomersApplication.java
@@ -0,0 +1,16 @@
+package com.vikram.customers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class CustomersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomersApplication.class, args);
+    }
+
+}
